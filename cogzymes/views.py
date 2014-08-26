d1fc941bbdaf81305dde3840c97430ee35e90ef5
Will Bryant <w.bryant@imperial.ac.uk>
2014-08-26 12:38:51 +0100

    Created skeleton of models views

--- cogzymes/views.py
+++ cogzymes/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+from annotation.models import Model_reaction, Source, Model_metabolite
+
+def model_contents(request):
+    return render_to_response('model_contents.html')
+
+def model(request, model_id = None):
+    
+    if model_id:
+        try:
+            model_source = Source.objects.get(id=model_id)
+        except:
+            return render_to_response('model_not_found.html')
+    
+    return render_to_response('model.html', {'model_source': model_source})
--- incognito/urls.py
+++ incognito/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'incognito.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^model/', 'cogzymes.views.model_contents'),
+    url(r'^model/id\:(?P<model_id>[^/]+)', 'cogzymes.views.model'),
     url(r'^admin/', include(admin.site.urls)),
 )
