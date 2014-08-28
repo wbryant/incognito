48ea00657f100cb943a0c3e9642b7d4727d77eff
Will Bryant <w.bryant@imperial.ac.uk>
2014-08-28 13:49:02 +0100

    Added to CSS/JS control files

--- static/js/incognito.js
+++ static/js/incognito.js
@@ -1,3 +1,27 @@
 /**
  * @author wbryant
  */
+
+$(document).ready( function() {
+
+    $('#table1').DataTable();
+    
+    $('table a').hover( 
+        function() {
+            $( this ).css("text-decoration","underline");
+        }, function() {
+            $( this ).css("text-decoration","none");
+        }
+    );
+
+    $("#top_menu a:not('.selected')").hover(
+        function() {
+            $( this ).addClass( "selected" );
+            $( this ).siblings( ".selected" ).css( "opacity", "0.6" );            
+        }, function() {
+            $( this ).removeClass( "selected" );
+            $( this ).siblings( ".selected" ).css( "opacity", "1" );
+        }
+    );
+   
+} );
