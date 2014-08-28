fe1f905ce66148fe2b4433ab3646f895ec8d6ccb
Will Bryant <w.bryant@imperial.ac.uk>
2014-08-28 13:49:51 +0100

    Finished home page and got top_menu navigation working

--- cogzymes/views.py
+++ cogzymes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from annotation.models import Model_reaction, Source, Model_metabolite
 from cogzymes.models import Reaction_pred
 
-def model_contents(request):
+def home(request):
     
     model_data = []
     for model in Source.objects.filter(organism__isnull = False):
@@ -15,24 +15,34 @@ def model_contents(request):
         model_dict['Number_of_Metabolites'] = Model_metabolite.objects.filter(source=model).count()
         model_dict['Number_of_Predictions'] = Reaction_pred.objects.filter(dev_model=model).count()
         model_data.append(model_dict)
-        print("{}, {}, {}".format(
-            model_data[-1]['model_id'],
-            model_data[-1]['tax_id'],
-            model_data[-1]['Number_of_Predictions']
-        ))
+#         print("{}, {}, {}".format(
+#             model_data[-1]['model_id'],
+#             model_data[-1]['tax_id'],
+#             model_data[-1]['Number_of_Predictions']
+#         ))
         
     if len(model_data) == 0:
         model_data = None
     
-    return render_to_response('model_contents.html', {'model_data': model_data})
+    return render_to_response('home.html', {'model_data': model_data})
     
 
 def model(request, model_id = None):
     
     if model_id:
         try:
-            model_source = Source.objects.get(id=model_id)
+            source = Source.objects.get(name=model_id)
         except:
-            return render_to_response('model_not_found.html')
+            return render_to_response('model_not_found.html', {'model_id': model_id})
     
-    return render_to_response('model.html', {'model_source': model_source})
+    model_reactions = Model_reaction.objects.filter(source=source)
+    reaction_preds = Reaction_pred.objects.filter(dev_model=source)
+    
+    return render_to_response('model.html', {
+        'source': source, 
+        'model_rxns': model_reactions, 
+        'pred_rxns': reaction_preds
+    })
+
+def model_unselected(request):
+    return render_to_response('model_unselected.html')
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates')
+]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -95,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = '/Users/wbryant/git/incognito/static/'
+STATIC_ROOT = '/Users/wbryant/git/incognito/static_root/'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/Users/wbryant/git/incognito/media/'
